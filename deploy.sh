ac900c65bfad163704c47d47aba87af579c781a2
risarisato <nqg44226@gmail.com>
2022-03-29 23:51:45 +0900

    git pullができいるか確認

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 # 前回のSCPコマンドを使ってレンタルサーバにファイルをアップロード
-# scp -r ./ risarisato@s223.xrea.com:~/lp-practice-static
+scp -r ./ risarisato@s223.xrea.com:~/lp-practice-static
 
 # 今回の同期させるコマンド
-rsync -av ./ risarisato@s223.xrea.com:~/lp-practice-static
+# rsync -av ./ risarisato@s223.xrea.com:~/lp-practice-static
+# これはWindowsでは出来ないので仮想環境かMACでやろう
